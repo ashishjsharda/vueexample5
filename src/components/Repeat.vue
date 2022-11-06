7981c654aeb3defab546ebd5623851438a014a6e
Ashish Sharda <ashishjsharda@gmail.com>
2022-11-06 16:35:06 -0600

    Add Repeat

---
+++ src/components/Repeat.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+ <span v-for="r in repetitions" :key="r">
+   {{content}}
+  </span>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Repeat"
+}
+</script>
+
+<style scoped>
+
+</style>
